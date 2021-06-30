9526e85349381e44dd4e95cb18ce0a5562e476f8
msseock <msseock@gmail.com>
2021-07-01 02:36:34 +0900

    implemented BMI calculation

--- BMICalc/BMICalc/ViewController.swift
+++ BMICalc/BMICalc/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightField: UITextField!
+    @IBOutlet weak var weightField: UITextField!
+    @IBOutlet weak var BMIField: UITextField!
+    
+    let numberFormatter:NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal // NumberFormatter.Style.decimal과 같음!
+        nf.minimumFractionDigits = 0
+        nf.maximumSignificantDigits = 3
+        return nf
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func bmiCalculation(_ sender: Any) {
+        if let heightText = heightField.text, let height = Double(heightText), let weightText = weightField.text, let weight = Double(weightText) {
+            // bmi = 체중 / 키^2
+            let bmi = weight / ((height/100) * (height/100))
+            BMIField.text = numberFormatter.string(from: NSNumber(value: bmi))
+        }
+    }
+    
+    @IBAction func textFieldFinishEdit(_ sender: Any) {
+        print("End!!")
+    }
 }
 
