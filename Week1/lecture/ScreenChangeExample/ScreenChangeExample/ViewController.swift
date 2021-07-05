2d7d631c18de022869836567f4ffd91c1241bfe6
msseock <msseock@gmail.com>
2021-07-06 01:26:01 +0900

    some directories changed

---
+++ Week1/lecture/ScreenChangeExample/ScreenChangeExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenChangeExample
+//
+//  Created by 석민솔 on 2021/07/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
